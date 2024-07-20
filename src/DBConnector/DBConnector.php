8410b2a29958e9a58e473bd4565490cb7b2cefe0
sierie85 <sierie85@users.noreply.github.com>
2024-07-20 20:36:48 +0200

    minor adjustments, add doc, preparing for release

--- src/DBConnector/DBConnector.php
+++ src/DBConnector/DBConnector.php
@@ -6,10 +6,28 @@ namespace Pool_CLI\DBConnector;
 use PDO;
 use PDOException;
 
-// todo: maybe mysqli is needed for production/dev?
+// todo: maybe mysqli is needed for production/dev? interface, abstract class?
 
+/**
+ * A class to handle database connections using PDO.
+ *
+ * This class is designed to establish a connection to a MySQL database using PDO.
+ * It encapsulates the connection details (host, port, user, password) and provides
+ * a method to connect to the database. The connection is configured to throw exceptions
+ * on error and to fetch data as associative arrays by default.
+ *
+ * Note: Consideration for mysqli extension usage is suggested for specific production or development needs.
+ */
 readonly class DBConnector
 {
+    /**
+     * Constructor to initialize the database connection parameters.
+     *
+     * @param string $host Database host address.
+     * @param string $port Database port number.
+     * @param string $user Database user name.
+     * @param string $password Database password.
+     */
     public function __construct(
         private string $host,
         private string $port,
@@ -19,6 +37,16 @@ readonly class DBConnector
     {
     }
 
+    /**
+     * Establishes a PDO connection to the database.
+     *
+     * This method attempts to create a new PDO connection using the provided
+     * credentials and configuration. It sets error handling to exception mode
+     * and the default fetch mode to associative array. If the connection fails,
+     * the method terminates the script and outputs the error message.
+     *
+     * @return PDO The PDO connection object on success.
+     */
     public function connect(): PDO
     {
         try {
--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -3,30 +3,29 @@ declare(strict_types=1);
 
 namespace Pool_CLI\Helper;
 
+/**
+ * Provides utility functions for the Pool CLI application.
+ */
 class Helper
 {
     /**
+     * Retrieves directories within a specified source directory that contain a specific indicator.
      *
-     * @param string $sourceDir
-     * @param string $indicator
-     * @return array
+     * This method scans the given source directory for subdirectories. It then checks each subdirectory
+     * to see if it contains a further subdirectory named according to the provided indicator. This is used,
+     * for example, to find project directories that contain a 'guis' or 'schemes' subdirectory, indicating
+     * that they are relevant to certain commands within the application.
+     *
+     * @param string $sourceDir The directory to scan for project directories.
+     * @param string $indicator The name of the subdirectory that indicates a relevant project directory.
+     * @return array An array of directory names that contain the indicator.
      */
     public static function getProjectDirs(string $sourceDir, string $indicator): array
     {
         $projectDirs = [];
-        $dirIterator = new \DirectoryIterator($sourceDir);
-        foreach ($dirIterator as $items) {
-            if ($items->isDir() && !$items->isDot()) {
-                $folderIterator = new \DirectoryIterator($sourceDir . '/' . $items->getFilename());
-                $isProject = false;
-                foreach ($folderIterator as $folder) {
-                    if ($folder->isDir() && !$folder->isDot() && $folder->getFilename() === $indicator) {
-                        $isProject = true;
-                    }
-                }
-                if ($isProject) {
-                    $projectDirs[] = $items->getFilename();
-                }
+        foreach (new \DirectoryIterator($sourceDir) as $dir) {
+            if ($dir->isDir() && !$dir->isDot() && is_dir("{$dir->getPathname()}/{$indicator}")) {
+                $projectDirs[] = $dir->getFilename();
             }
         }
         return $projectDirs;
