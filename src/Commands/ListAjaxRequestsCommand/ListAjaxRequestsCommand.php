3bdb3d2a5f0bbf8dc6130c794c47b3fb87b69f82
sierie85 <sierie85@users.noreply.github.com>
2024-07-20 20:20:33 +0200

    minor adjustments, add doc, preparing for release

--- src/Commands/ListAjaxRequestsCommand/ListAjaxRequestsCommand.php
+++ src/Commands/ListAjaxRequestsCommand/ListAjaxRequestsCommand.php
@@ -10,23 +10,43 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+/**
+ * Command to list all registered Ajax methods for GUIs in a selected project.
+ *
+ * This command allows users to view all Ajax methods that have been registered
+ * within the GUIs of a specified project. It scans the project directories for
+ * PHP files that register Ajax methods and displays them in a formatted table.
+ */
 class ListAjaxRequestsCommand extends Command
 {
+    /**
+     * Configures the command settings.
+     * Sets the name, description, and help message for the command.
+     *
+     * @return void
+     */
     protected function configure(): void
     {
         $this->setName('list:ajax')
-            ->setDescription('show all ajax_requests from guis in chosen project')
-            ->setHelp('lookup on pool-documentation/pool-cli how to list all ajax_requests from GUIs in project');
+            ->setDescription('show all registered ajaxMethods from guis in chosen project')
+            ->setHelp('lookup on pool-documentation/pool-cli how to list all ajaxMethods from GUIs in project');
     }
 
+    /**
+     * Executes the command to list all Ajax methods.
+     *
+     * @param InputInterface $input The input interface.
+     * @param OutputInterface $output The output interface.
+     * @return int The status code (0 for success, non-zero for failure).
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->title('<info>Show all ajax_requests of Project</info>');
+        $io->title('<info>Show all registered AjaxMethods of Project</info>');
 
         $projectDirs = Helper::getProjectDirs(SRC_DIR, 'guis');
-        $project = $io->choice('Of which project you want to see all ajax_requests?', $projectDirs);
+        $project = $io->choice('Of which project you want to see all ajaxMethods?', $projectDirs);
         $projectDir = SRC_DIR . '/' . $project . '/guis';
 
         $ajaxRequests = [];
@@ -35,7 +55,6 @@ class ListAjaxRequestsCommand extends Command
             if ($items->isDir() && !$items->isDot()) {
                 $fileIterator = new \DirectoryIterator($projectDir . '/' . $items->getFilename());
                 foreach ($fileIterator as $item) {
-                    // todo: get all php files? is this necessary? none gui load ajax?
                     if ($item->isFile() && $item->getExtension() === 'php' && str_starts_with($item->getFilename(), 'GUI_')) {
                         $file = file_get_contents($projectDir . '/' . $items->getFilename() . '/' . $item->getFilename());
                         $matches = [];
